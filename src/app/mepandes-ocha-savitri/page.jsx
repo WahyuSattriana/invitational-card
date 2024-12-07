162fefb5a5869cf2b7baa8a0252cb491b1fc0c0a
wahyusattriana <wahyusattriana@gmail.com>
2024-12-07 21:57:46 +0800

    add small scrollreveal

--- src/app/mepandes-ocha-savitri/page.jsx
+++ src/app/mepandes-ocha-savitri/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import hero from "./images/hero.jpg";
 import ochasm from "./images/ocha-sm.jpg";
@@ -16,22 +18,22 @@ import gal5 from "./images/gal5.jpg";
 import gal6 from "./images/gal6.jpg";
 import Autoplay from "./Autoplay";
 
-import { RevealWrapper } from  'next-reveal'
+import { RevealList, RevealWrapper } from  'next-reveal'
 
 const mepandesOchaSavitri = () => {
   return (
       <div className="box-border">
         <header className="h-screen flex flex-col justify-center items-center">
-          <div className="flex flex-col items-center gap-10 my-10">
+          <RevealWrapper className="flex flex-col items-center gap-10 my-10">
             <h1 className="font-aboreto text-xl text-white">MEPANDES</h1>
-            <div className="flex flex-col items-center gap-8">
+            <RevealList interval={60} delay={500} className="flex flex-col items-center gap-8">
               <h1 className="font-cinzel text-4xl text-white">Ocha Chandra</h1>
               <h1 className="font-cinzel text-4xl text-white">&</h1>
               <h1 className="font-cinzel text-4xl text-white">Savitri Candrika</h1>
-            </div>
+            </RevealList>
             <h1 className="font-aboreto text-xl text-white">13 / 12 / 2024</h1>
-          </div>
-          <p className="text-center font-baskervville m-5 text-white">Kami sangat mengharapkan kehadiran <br/> Bapak/Ibu/Saudara/i</p>
+          </RevealWrapper>
+          <RevealWrapper><p className="text-center font-baskervville m-5 text-white">Kami sangat mengharapkan kehadiran <br/> Bapak/Ibu/Saudara/i</p></RevealWrapper>
           {/* <div onClick={"#section-one"} className="cursor-pointer px-3 py-1 bg-zinc-500 opacity-100 text-white rounded-md bg-opacity-50">V</div> */}
 
           <Image fill alt="hero" quality={100} src={hero} sizes="cover" className="object-cover -z-20 absolute top-0 brightness-50"/>
@@ -39,14 +41,14 @@ const mepandesOchaSavitri = () => {
 
         <section id="section-one" className="min-h-screen bg-transparent flex items-center justify-center relative">
           <div className="flex items-center justify-center flex-col gap-10 my-5">
-            <div className="grid grid-cols-2 gap-1 w-80 h-56">
+            <RevealList className="grid grid-cols-2 gap-1 w-80 h-56">
               <div className="relative">
                 <Image alt="ocha" src={ochasm} fill className="rounded-md absolute object-cover"/>
               </div>
               <div className="relative">
                 <Image alt="savitri" src={savitrism} fill className="rounded-md object-cover absolute"/>
               </div>
-            </div>
+            </RevealList>
             <h1 className="font-aboreto text-xl">OM SWASTYASTU</h1>
             <p className="text-center font-baskervville max-w-96 text-sm sm:text-base">Atas Asung Kerta Wara Nugraha Ida Sang <br/> Hyang Widhi Wasa / Tuhan Yang Maha <br/> Esa, Kami bermaksud menyelenggarakan<br/> Acara Mepandes pada:</p>
             <div className="flex flex-col justify-center items-center gap-3 text-sm">
